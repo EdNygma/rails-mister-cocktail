c65be60e239c12528741f536c903238c1169027d
kevin mormin <kevin_mormin_student2018@wilder.school>
2019-05-16 16:37:25 +0200

    controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/show'
+  get 'doses/new'
+  get 'doses/edit'
   resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
