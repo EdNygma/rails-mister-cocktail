64edd956589be13fcf3bc80580ca2636a4295c7b
kevin mormin <kevin_mormin_student2018@wilder.school>
2019-05-16 17:40:21 +0200

    config & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'doses/index'
-  get 'doses/show'
-  get 'doses/new'
-  get 'doses/edit'
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  
+  resources :doses,  only:[:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
